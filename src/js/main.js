a98d0204df7ad48754da1776fe7fb79a57d7c763
michaelparkernorton <michaelparkernorton.va@gmail.com>
2023-12-14 12:31:41 -0300

    updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+let height = 0;
+
 document.body.onkeydown = function (e) {
   if (e.key == ' ' || e.code == 'Space' || e.keyCode == 32) {
 		if (document.fullscreenElement) {
@@ -5,5 +7,20 @@ document.body.onkeydown = function (e) {
 		} else {
 			document.body.requestFullscreen();
 		}
+  }
+	if (e.key == 'ArrowDown' || e.code == 'ArrowDown') {
+		height += window.innerHeight
+    window.scroll({
+			top: height,
+			behavior: "smooth",
+		});
+  }
+	if (e.key == 'ArrowUp' || e.code == 'ArrowUp') {
+		console.log(window.innerHeight);
+		height -= window.innerHeight;
+    window.scroll({
+			top: height,
+			behavior: "smooth",
+		});
   }
 };
