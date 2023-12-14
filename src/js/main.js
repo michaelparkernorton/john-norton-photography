e300445f48383ca11b62bafb2d3596fcf9ebddba
michaelparkernorton <michaelparkernorton.va@gmail.com>
2023-12-14 12:40:28 -0300

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
 let height = 0;
+const colors = document.querySelectorAll(".color")
+console.log(colors[1]);
+let count = 0;
 
 document.body.onkeydown = function (e) {
   if (e.key == ' ' || e.code == 'Space' || e.keyCode == 32) {
@@ -9,18 +12,23 @@ document.body.onkeydown = function (e) {
 		}
   }
 	if (e.key == 'ArrowDown' || e.code == 'ArrowDown') {
-		height += window.innerHeight
-    window.scroll({
-			top: height,
-			behavior: "smooth",
-		});
+		count += 1;
+		colors[count].scrollIntoView();
+		// height += window.innerHeight
+    // window.scroll({
+		// 	top: height,
+		// 	behavior: "smooth",
+		// });
   }
 	if (e.key == 'ArrowUp' || e.code == 'ArrowUp') {
-		console.log(window.innerHeight);
-		height -= window.innerHeight;
-    window.scroll({
-			top: height,
-			behavior: "smooth",
-		});
+		count -= 1;
+		colors[count].scrollIntoView();
+
+		// console.log(window.innerHeight);
+		// height -= window.innerHeight;
+    // window.scroll({
+		// 	top: height,
+		// 	behavior: "smooth",
+		// });
   }
 };
